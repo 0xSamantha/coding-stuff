e9b42dc533fd79680106edb40d7d264bcf1fb5d5
0xSamantha <samchavoya@gmail.com>
2024-04-10 20:40:07 -0700

    solidity - if statements, contracts

--- javascript/script.js
+++ javascript/script.js
@@ -17,24 +17,28 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// location controls
 const locations = [
     {
       name: "town square",
       "button text": ["Go to store", "Go to cave", "Fight dragon"],
       "button functions": [goStore, goCave, fightDragon],
-      "text": "You are in the town square. You see a sign that says \"Store\"." }
+      text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+      name: "store",
+      "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+      "button functions": [buyHealth, buyWeapon, goTown],
+      text: "You enter the store."
+    }
   ];
 
-// initialize buttons
+// initializing buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-
-}
-
-function goTown() {
     button1.innerText = "Go to store";
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
@@ -44,17 +48,13 @@ function goTown() {
     text.innerText = "You are in the town square. You see a sign that says \"Store\".";
   }
   
+  function goTown() {
+    update(locations[0]);
+  }
+  
+  function goStore() {
+  }
 
-function goStore() {
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-    text.innerText = "You enter the store."
-}
-    }
 
 function goCave() {
     console.log("Going to cave.");
---
+++ solidity/ifstatements.sol
@@ -0,0 +1,18 @@
+function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
+}
