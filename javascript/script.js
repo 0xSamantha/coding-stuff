24befe4415afb6b8816f66403e57dccbfeaf5f35
0xSamantha <samchavoya@gmail.com>
2024-04-13 20:48:35 -0700

    more mods

--- javascript/script.js
+++ javascript/script.js
@@ -91,9 +91,19 @@ function update(location) {
   }
   
   function buyWeapon() {
-    if (gold >= 30) {
-      gold -= 30;
+    if (currentWeapon < weapons.length) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  }
   }
 
   function fightSlime() {
