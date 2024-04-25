c3dd6d5721e6791365424d591fa5c861771604b4
0xSamantha <samchavoya@gmail.com>
2024-04-25 00:56:20 -0700

    more js

--- JS - Calorie Counter/script.js
+++ JS - Calorie Counter/script.js
@@ -27,8 +27,33 @@ function addEntry() {
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />`;
-    targetInputContainer.innerHTML += HTMLString;
+    <input
+      type="number"
+      min="0"
+      id="${entryDropdown.value}-${entryNumber}-calories"
+      placeholder="Calories"
+    />`;
+    targetInputContainer.insertAdjacentHTML();
   }
 
-addEntryButton.addEventListener('click', addEntry);
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+  }
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        let invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+          }
+          calories += Number(currVal);
+    }
+    return calories;
+  }
+
+addEntryButton.addEventListener("click", addEntry);
