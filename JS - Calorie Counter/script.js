afebdf7ce087b4c63aeaee4eb64fc114f2b1caf7
0xSamantha <samchavoya@gmail.com>
2024-04-28 22:27:16 -0700

    calorie input

--- JS - Calorie Counter/script.js
+++ JS - Calorie Counter/script.js
@@ -33,22 +33,25 @@ function addEntry() {
       id="${entryDropdown.value}-${entryNumber}-calories"
       placeholder="Calories"
     />`;
-    targetInputContainer.insertAdjacentHTML();
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
   }
 
 function calculateCalories(e) {
     e.preventDefault();
     isError = false;
+  
     const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
     const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
     const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
     const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
     const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
-  }
-
-function calculateCalories(e) {
-    e.preventDefault();
-    isError = false;
+  
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
   }
 
 function getCaloriesFromInputs(list) {
